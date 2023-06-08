c5457e85f9e21fd28f023615a06ded8531d4d29f
sagaseta22 <sagaseta22@gmail.com>
2023-06-08 21:05:34 +0200

    Caluculadora.V2
    Multiplicación y división.

--- Calculador.java
+++ Calculador.java
@@ -10,6 +10,16 @@ public class Calculador {
         return num1 - num2;
     }
   
-
-
+    public static double multiplicacion(double num1, double num2) {
+        return num1 * num2;
+    }
+  
+    public static double division(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new IllegalArgumentException("No se puede dividir entre cero");
+        }
+    }
+  
 }
