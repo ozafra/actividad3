a6bd7c842f7134c25572eb1825ea4c968f862dac
Igor <igoreslein@gmail.com>
2023-06-08 20:50:41 +0200

    CalculadoraV1
    Suma y resta

---
+++ Calculador.java
@@ -0,0 +1,15 @@
+package Calculadora;
+
+public class Calculador {
+	
+	public static double suma(double num1, double num2) {
+        return num1 * num2;
+    }
+  
+    public static double resta(double num1, double num2) {
+        return num1 - num2;
+    }
+  
+
+
+}
